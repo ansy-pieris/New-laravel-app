d91f32c4c71f9c28afae800f0547401d15517041
Ansy Pieris <cb014983@students.apiit.lk>
2025-10-07 07:11:08 +0530

    fix: Remove CORS middleware from kernel.php as images now served via dedicated route

--- app/Http/kernel.php
+++ app/Http/kernel.php
@@ -4,8 +4,6 @@ class Kernel extends \Illuminate\Foundation\Http\Kernel
 {
     protected $middlewareGroups = [
         'api' => [
-            // Removed EnsureFrontendRequestsAreStateful for true API usage
-            \Fruitcake\Cors\HandleCors::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];
