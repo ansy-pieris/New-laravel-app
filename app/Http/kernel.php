d63d8d08980f14926e079144f9ea00b98ca36c5e
Ansy Pieris <cb014983@students.apiit.lk>
2025-10-13 10:33:23 +0530

    Update kernel.php

--- app/Http/kernel.php
+++ app/Http/kernel.php
@@ -4,6 +4,7 @@ class Kernel extends \Illuminate\Foundation\Http\Kernel
 {
     protected $middlewareGroups = [
         'api' => [
+            \Fruitcake\Cors\HandleCors::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];
